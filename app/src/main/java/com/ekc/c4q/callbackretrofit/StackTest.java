4d6d30173d9b583656c77d353bb602c94b7cffa0
Erick Chang <ekc.dev@gmail.com>
2016-10-29 17:37:42 -0400

    Rename queue to stack in test

--- app/src/main/java/com/ekc/c4q/callbackretrofit/StackTest.java
+++ app/src/main/java/com/ekc/c4q/callbackretrofit/StackTest.java
@@ -6,20 +6,20 @@ import java.util.Stack;
 public class StackTest {
   public static void main(String[] args) {
     MyStack myStack = new MyStack();
-    Stack<Integer> queue = new Stack<>();
+    Stack<Integer> stack = new Stack<>();
 
     System.out.println("Adding 1");
     myStack.push(1);
-    queue.push(1);
+    stack.push(1);
     System.out.println("Adding 2");
     myStack.push(2);
-    queue.push(2);
+    stack.push(2);
     System.out.println("Adding 3");
     myStack.push(3);
-    queue.push(3);
+    stack.push(3);
 
-    System.out.printf("Remove: %d == %d ? \n", myStack.pop(), queue.pop());
-    System.out.printf("Remove: %d == %d ? \n", myStack.pop(), queue.pop());
-    System.out.printf("Remove: %d == %d ? \n", myStack.pop(), queue.pop());
+    System.out.printf("Remove: %d == %d ? \n", myStack.pop(), stack.pop());
+    System.out.printf("Remove: %d == %d ? \n", myStack.pop(), stack.pop());
+    System.out.printf("Remove: %d == %d ? \n", myStack.pop(), stack.pop());
   }
 }
